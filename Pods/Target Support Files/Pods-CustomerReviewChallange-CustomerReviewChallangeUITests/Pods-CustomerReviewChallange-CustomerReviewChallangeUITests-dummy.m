a272f07e81467503b38974747e79e5af6ba86d50
erkutbas <ebas1@binghamton.edu>
2020-08-07 01:52:09 +0300

    pods removed

--- Pods/Target Support Files/Pods-CustomerReviewChallange-CustomerReviewChallangeUITests/Pods-CustomerReviewChallange-CustomerReviewChallangeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomerReviewChallange_CustomerReviewChallangeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomerReviewChallange_CustomerReviewChallangeUITests
-@end
